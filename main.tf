fbb886b2af5fd8f585a2008e4441b862d0fb7b46
Mohit Saini <mohit.maharan@gmail.com>
2025-07-21 10:08:16 +0530

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.37.0"
+      
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+provider "azurerm" {
+  features {}
+  subscription_id = "ba3dd1c9-2ade-4648-a13f-65581e2a2874"
+}
+
+resource "azurerm_resource_group" "pipeline-rg" {
+  name     = "pipeline-rg"
+  location = "central india"
+}
