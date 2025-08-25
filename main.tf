6b04c0690e840768fddb0706b3f1a777464101de
Mohit Saini <mohit.maharan@gmail.com>
2025-08-25 17:27:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  subscription_id = "ba3dd1c9-2ade-4648-a13f-65581e2a2874"
+  subscription_id = "f0c6c28a-7429-42b3-aefa-63439e20a651"
 }
 
 resource "azurerm_resource_group" "pipelinee-rg" {
