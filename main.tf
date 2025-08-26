97d13a49eea5f5cb7e766f9f95a6242dfafe4adf
Mohit Saini <mohit.maharan@gmail.com>
2025-08-26 20:19:58 +0530

    rg

--- main.tf
+++ main.tf
@@ -1,20 +1,23 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.37.0"
-      
     }
   }
 
+  backend "azurerm" {}
+
   required_version = ">= 1.5.0"
 }
+
 provider "azurerm" {
   features {}
   subscription_id = "f0c6c28a-7429-42b3-aefa-63439e20a651"
 }
 
-resource "azurerm_resource_group" "pipelinee-rg" {
-  name     = "pipelinee-rg"
+resource "azurerm_resource_group" "test-pipeline-rg" {
+  name     = "test-pipeline-rg"
   location = "central india"
 }
+
